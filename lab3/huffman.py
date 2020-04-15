1850489888f77df223110524943c5e28fdb1099a
pan-maciek <kozieja.maciej@gmail.com>
2020-04-16 00:19:15 +0200

    preparation for improvements
    implementing tree shape storing algorithm

--- lab3/huffman.py
+++ lab3/huffman.py
@@ -2,91 +2,89 @@ from collections import Counter
 from heapq import heappop, heappush
 from bitarray import bitarray
 from array import array
+from dataclasses import dataclass, field
+from typing import Union
 
-class Huffman:
-    class Node:
-        is_leaf = False
-        def __init__(self, low, high):
-            self.low  = low
-            self.high = high
-            self.count = low.count + high.count
-            
-        def __lt__(self, other):
-            return self.count < other.count
+@dataclass
+class CmpNode:
+    count: int
+    def __lt__(self, other):
+        return self.count < other.count
 
-    class Leaf:
-        is_leaf = True
-        def __init__(self, char, count):
-            self.char  = char
-            self.count = count
+@dataclass
+class Leaf(CmpNode):
+    is_leaf = True
+    char: chr
 
-        def __lt__(self, other):
-            return self.count < other.count
+@dataclass
+class Node(CmpNode):
+    is_leaf = False
+    low: CmpNode
+    high: CmpNode
 
-    def __init__(self, freq=None, unicode=True):
-        self.frequency = Counter(freq) if freq != None else Counter()
-        self._codes= None
-        self.frequency_changed = True
-        self.ascii_mode = not unicode
+def huffman(frequency):
+    heap, freq = [], Counter(frequency)
 
-    @property
-    def array_mode(self):
-        return 'B' if self.ascii_mode else 'H'
+    for value, count in freq.items():
+        heappush(heap, Leaf(count, value))
+    
+    while len(heap) > 1:
+        low = heappop(heap)
+        high = heappop(heap)
+        heappush(heap, Node(low.count + high.count, low, high))
 
-    @property
-    def codes(self):
-        if not self.frequency_changed:
-            return self._codes
+    return Node(heap[0].count, heap[0], Leaf(0, '#')) if heap[0].is_leaf else heap[0]
 
-        heap = []
-        for value, count in self.frequency.items():
-            heappush(heap, Huffman.Leaf(value, count))
-        
-        while len(heap) > 1:
-            low = heappop(heap)
-            high = heappop(heap) 
-            heappush(heap, Huffman.Node(low, high))
-        
-        def traverse(root, path):
-            if root.is_leaf:
-                yield root.char, path
-            else:
-                yield from traverse(root.low , path + '0')
-                yield from traverse(root.high, path + '1')
-        
-        self._codes = dict(traverse(heap[0], bitarray(0))) \
-                if not heap[0].is_leaf else dict([(heap[0].char, bitarray('0'))])
-        self.frequency_changed = False
-        return self._codes
+def mk_codes(root):
+    def traverse(root, path):
+        if root.is_leaf:
+            yield root.char, path
+        else:
+            yield from traverse(root.low, path + '0')
+            yield from traverse(root.high, path + '1')
+    
+    return dict(traverse(root, bitarray()))
 
-    def encode(self, text):
-        b = bitarray()
-        b.encode(self.codes, text)
-        return b
+def encode(text, frequency=None, tree=None, codes=None):
+    codes = codes or mk_codes(tree or huffman(frequency or text))
+    encoded = bitarray()
+    for c in text: 
+        encoded += codes[c]
+    return encoded
 
-    def decode(self, b):
-        return ''.join(b.decode(self.codes))
+def decode(bits, root):
+    iter, out = root, ""
+    for b in bits:
+        iter = iter.high if b else iter.low
+        if iter.is_leaf:
+            out += iter.char
+            iter = root
+    return out
 
-    def tofile(self, file, text):
-        encoded = self.encode(text)
-        meta = array(self.array_mode, [len(encoded) % 8, len(self.frequency)])
-        meta.extend(self.frequency.values())
-        meta.extend(map(ord, self.frequency.keys()))
-        meta.tofile(file)
-        encoded.tofile(file)
+def tofile(file, text, frequency=None, tree=None, codes=None):
+    freq = frequency or Counter(text)
+    alphabet_bytes =  array('B', (''.join(freq.keys())).encode())
+    encoded = encode(text, freq, tree, codes)
+    array('H', [ len(alphabet_bytes), len(encoded) % 8 ]).tofile(file)
+    alphabet_bytes.tofile(file)
+    array('H', freq.values()).tofile(file)
 
-    def fromfile(self, file):
-        meta = array(self.array_mode)
-        meta.fromfile(file, 2)
-        text_trim, alphabet_size = meta
+    encoded.tofile(file)
 
-        meta.fromfile(file, 2 * alphabet_size)
-        alphabet, count = map(chr, meta[-alphabet_size:]), meta[2:2+alphabet_size]
-        newfrequency = Counter(dict(zip(alphabet, count)))
-        if newfrequency != self.frequency:
-            self.frequency_changed = True
-            self.frequency = newfrequency
+def fromfile(file):
+    metadata = array('H')
+    metadata.fromfile(file, 2)
+    alphabet_bytes_size, encoded_excess = metadata
 
-        text = bitarray()
-        text.fromfile(file)
-        return self.decode(text[:-(8-text_trim)] if text_trim else text)
+    alphabet_bytes = array('B')
+    alphabet_bytes.fromfile(file, alphabet_bytes_size)
+    alphabet = alphabet_bytes.tobytes().decode()
+    frequency = array('H')
+    frequency.fromfile(file, len(alphabet))
+
+    frequency = Counter(dict(zip(alphabet, frequency)))
+    encoded = bitarray()
+    encoded.fromfile(file)
+    encoded = encoded[:-encoded_excess] if encoded_excess else encoded
+
+    return decode(encoded, huffman(frequency))
--- lab3/test.py
+++ lab3/test.py
@@ -1,7 +1,10 @@
-from huffman import Huffman
+from huffman import tofile, fromfile, huffman, mk_codes
+from old import Huffman
+import os
 
 for p in ['1KB', '10KB', '100KB', '1MB']:
     with open(f"{p}.txt", "r") as file:
         x = file.read()
         with open(f"{p}.c.txt", "wb") as file:
-            Huffman(x).tofile(file, x)
+            tofile(file, x)
+        print(p, 1 - (os.stat(f"{p}.c.txt").st_size / os.stat(f"{p}.txt").st_size))
