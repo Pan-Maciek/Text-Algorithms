cf6f8f3eabb786e93cabd5dc504e3946aa72e6be
Pan-Maciek <koziejka.com@gmail.com>
2020-05-09 14:10:05 +0200

    reduced memory usage of distance from O(n^2) to O(2n)

--- lab4/distance.py
+++ lab4/distance.py
@@ -4,24 +4,25 @@ from dataclasses import dataclass
 def delta(a, b):
     return 0 if a == b else 1
 
-def mk_transition_matrix(a, b, dt=delta):
-    x, y = len(a) + 1, len(b) + 1
+def distance(a, b, dt=delta):
+    a_len, b_len = len(a), len(b)
+    if a_len > b_len:
+        a, b = b, a
+        a_len, b_len = b_len, a_len
+    
+    row1 = np.fromiter(range(a_len + 1), count=a_len + 1, dtype=float)
+    row2 = np.empty(a_len + 1, dtype=float)
 
-    m = np.empty((x, y), dtype=int)
-    m[:, 0], m[0, :] = range(x), range(y) # initialize edges
-
-    for x, y in np.ndindex((x-1, y-1)):
-        m[x+1, y+1] = min(
-            m[x, y] + dt(a[x], b[y]), # update
-            m[x+1, y] + 1, # insert
-            m[x, y+1] + 1  # delete
-        )
-
-    return m
-
-def distance(a, b, dt=delta, m=None):
-    m = m or mk_transition_matrix(a, b, dt)
-    return m[len(a), len(b)]
+    for x in range(b_len):
+        row2[0] = x + 1
+        for y in range(a_len):
+            row2[y+1] = min(
+                row1[y] + dt(a[y], b[x]), 
+                row1[y+1] + 1, 
+                row2[y] + 1
+            )
+        row1, row2 = row2, row1
+    return row1[-1]
 
 @dataclass
 class Insert:
@@ -57,9 +58,23 @@ class Substitute:
     def undo(self, str):
         return str[:self.at] + self.old + str[self.at+1:]
 
+def mk_transition_matrix(a, b, dt=delta):
+    x, y = len(a) + 1, len(b) + 1
 
-def transition(a, b, dt=delta, m=None):
-    m = m or mk_transition_matrix(a, b, dt)
+    m = np.empty((x, y), dtype=float)
+    m[:, 0], m[0, :] = range(x), range(y) # initialize edges
+
+    for x, y in np.ndindex((x-1, y-1)):
+        m[x+1, y+1] = min(
+            m[x, y] + dt(a[x], b[y]), # update
+            m[x+1, y] + 1, # insert
+            m[x, y+1] + 1  # delete
+        )
+
+    return m
+
+def transition(a, b, dt=delta):
+    m = mk_transition_matrix(a, b, dt)
     x, y = len(a), len(b)
 
     while (x, y) != (0, 0):
